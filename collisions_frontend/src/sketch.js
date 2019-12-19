647579bbab6cc2b9b1cc5da8e9087415f0030a52
mrh9ud <mrh9ud@virginia.edu>
2019-12-19 10:15:52 -0500

    canvas and ground rendered

---
+++ collisions_frontend/src/sketch.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("DOM loaded")
+    setup()
+    draw()
+})
+
+let ground
+
+function setup() {
+    createCanvas(600, 400)
+    ground = new Box(0, height-20, width, 20)
+}
+        
+function draw() {
+    background(0)
+    ground.show()
+}
